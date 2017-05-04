8734b767bd88fb504212624305f093aae38cb927
cai <b4400440077@gmailc.om>
2017-05-04 16:33:17 +0800

    git remote add origin https://github.com/h94/vue.git

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div class="nav">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/hello">Hello</router-link>
+  </div>
+  <transition name="fade" mode="out-in">
+    <keep-alive><router-view class="view"></router-view></keep-alive>
+  </transition>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+
