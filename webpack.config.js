77e05780a85135aa601fa3d5fed827d929da0715
cai <b4400440077@gmail.com>
2017-05-04 17:13:55 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var path = require('path')
+var webpack = require('webpack')
+var ClosureCompilerPlugin = require('webpack-closure-compiler')
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf)$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new ClosureCompilerPlugin({
+      compiler: {
+        language_in: 'ECMASCRIPT6',
+        language_out: 'ECMASCRIPT5',
+        compilation_level: 'SIMPLE'/*,
+        create_source_map: true*/
+      },
+      concurrency: 3
+    })
+  ])
+}
